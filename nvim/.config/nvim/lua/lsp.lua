7112be577b46e57cfdb2a88a1bc4248eb7ba686e
snomanners <121182673+snomanners@users.noreply.github.com>
2025-05-31 16:28:29 +0200

    Fix broken mason_lspconfig setup_handlers

--- nvim/.config/nvim/lua/lsp.lua
+++ nvim/.config/nvim/lua/lsp.lua
@@ -44,16 +44,9 @@ mason_lspconfig.setup {
     ensure_installed = vim.tbl_keys(servers),
 }
 
-mason_lspconfig.setup_handlers {
-    function(server_name)
-        require('lspconfig')[server_name].setup {
-            capabilities = capabilities,
-            on_attach = on_attach,
-            settings = servers[server_name],
-            filetypes = (servers[server_name] or {}).filetypes,
-            root_dir = function()
-                return vim.fn.getcwd()
-            end,
-        }
-    end,
-}
+-- replacement for mason_lspconfig.setuphandlers
+vim.lsp.config("*", {
+    capabilities = capabilities,
+    on_attach = on_attach,
+    handlers = handlers,
+})
